dbec27737e0eefa84edd783c23c850f6880dbc57
Mehwish Aziz <mehwisha@msn.com>
2018-07-24 02:02:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)# code an argument here
+i = 0
+while i < names.length
+  return names[i] if yield(names[i])
+  i = i + 1
+end
 end
 
 def summon_captain_planet# code an argument here
